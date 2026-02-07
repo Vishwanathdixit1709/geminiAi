135a08b280d50ddd6a28f04a38dd74a554e83216
vishwanathdixit <dixitvishwa1709@gmail.com>
2026-02-07 13:34:52 +0530

    Fixed mobile responsiveness and added prompt suggestions

--- src/components/main-component/Main.jsx
+++ src/components/main-component/Main.jsx
@@ -7,7 +7,34 @@ import { Context } from '../../context/Context.jsx'
 const Main =()=> {
 
 const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
-
+  // ADD THIS ARRAY
+  const promptSuggestions = [
+    {
+      text: "Suggest beautiful places to see on an upcoming road trip",
+      icon: assets.compass_icon
+    },
+    {
+      text: "Briefly summarize this concept: urban planning", 
+      icon: assets.bulb_icon
+    },
+    {
+      text: "Brainstorm team bonding activities for our work retreat",
+      icon: assets.message_icon
+    },
+    {
+      text: "Improve the readability of the following code",
+      icon: assets.code_icon
+    },
+    // You can easily add more here!
+    {
+      text: "Explain React hooks with simple examples",
+      icon: assets.code_icon
+    },
+    {
+      text: "Create a study plan for data structures",
+      icon: assets.bulb_icon
+    }
+  ];
 
   return (
     <div className='main'>
@@ -23,24 +50,21 @@ const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useCo
                 <p><span>Hello, Dev..</span></p>
                 <p>How can i help you today</p>
             </div>
-            <div className='cards'>
-                <div className='card'>
-                    <p>Suggest beautiful place to see on an upcoming road trip </p>
-                    <img src={assets.compass_icon}></img>
+            <div className="cards">
+              {promptSuggestions.slice(0, 4).map((suggestion, index) => (
+                <div 
+                  key={index} 
+                  className="card" 
+                  onClick={() => onSent(suggestion.text)}
+                >
+                  <p>{suggestion.text}</p>
+                  <img src={suggestion.icon} alt="" />
                 </div>
-                <div className='card'>
-                    <p> Briefly summarize this concept: urban planning</p>
-                    <img src={assets.bulb_icon}></img>
-                </div>
-                <div className='card'>
-                    <p> Brainstrom team bonding activities for our work retreat  </p>
-                    <img src={assets.message_icon}></img>
-                </div>
-                <div className='card'>
-                    <p> improve the readability of the following code </p>
-                    <img src={assets.code_icon}></img>
-                </div>
+              ))}
             </div>
+
+           
+
             
             </>
             : <div className='result'>
